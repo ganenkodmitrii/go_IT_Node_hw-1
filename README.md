7018f257d6dafb0fd0fcb225b76922c8221495f6
Ganenco Dmitri <ganenko.dmitrii@gmail.com>
2022-04-17 12:41:25 +0300

    Updated addContact on invokeAction

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # go_IT_Node_hw-1
 
-npx ts-node index.ts  --action list => https://ibb.co/D8ZqYx0
-npx ts-node index.ts  --action get --id 5 => https://ibb.co/sHxTvpc
-npx ts-node index.ts  --action add --name Mango --email mango@gmail.com --phone 322-22-22 => https://ibb.co/52B7Yqm
-npx ts-node index.ts  --action remove --id=3 => https://ibb.co/JCQcfnn
+npx ts-node index.ts --action list
+npx ts-node index.ts --action get --id 5
+npx ts-node index.ts --action add --name Mango --email mango@gmail.com --phone 322-22-22
+npx ts-node index.ts --action remove --id 22626
--- api/contacts.ts
+++ api/contacts.ts
@@ -1,53 +1,62 @@
-const fs = require('fs').promises
-const path = require('path')
-const generateUniqueId = require('generate-unique-id')
+const fs = require("fs").promises;
+const path = require("path");
+const generateUniqueId = require("generate-unique-id");
 
-const contactsPath = path.join(__dirname, '../db/contacts.json')
-
+const contactsPath = path.join(__dirname, "../db/contacts.json");
+console.log(contactsPath);
 export async function listContacts() {
   try {
-    return JSON.parse(await fs.readFile(contactsPath))
+    return JSON.parse(await fs.readFile(contactsPath));
   } catch (err) {
-    return err.message
+    return err.message;
   }
 }
 
 export async function getContactById(contactId) {
+  const numberId = Number(contactId);
   try {
-    const foundContact = JSON.parse(await fs.readFile(contactsPath)).find((contact) => contact.id === contactId)
-    return foundContact || 'Contact not found'
+    const foundContact = JSON.parse(await fs.readFile(contactsPath)).find(
+      (contact) => contact.id === numberId
+    );
+    return foundContact || "Contact not found";
   } catch (err) {
-    return err.message
+    return err.message;
   }
 }
 
 export async function searchContact(value) {
   try {
-    const foundContact = JSON.parse(await fs.readFile(contactsPath)).find((contact) => contact.name.includes(value))
+    const foundContact = JSON.parse(await fs.readFile(contactsPath)).find(
+      (contact) => contact.name.includes(value)
+    );
 
-    return foundContact || 'Contact not found'
+    return foundContact || "Contact not found";
   } catch (err) {
-    return err.message
+    return err.message;
   }
 }
 
 export async function removeContact(contactId) {
+  const numberId = Number(contactId);
   try {
-    const contacts = JSON.parse(await fs.readFile(contactsPath)).filter((contact) => contact.id !== contactId)
+    const contacts = JSON.parse(await fs.readFile(contactsPath)).filter(
+      (contact) => contact.id !== numberId
+    );
 
-    fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2))
+    fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
   } catch (err) {
-    return err.message
+    return err.message;
   }
 }
 
 export async function addContact({ id: defaultId, name, email, phone }) {
   try {
-    const contacts = JSON.parse(await fs.readFile(contactsPath))
-    const id = defaultId || Number(generateUniqueId({ length: 5, useLetters: false }))
-    contacts.push({ id, name, email, phone })
-    fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2))
+    const contacts = JSON.parse(await fs.readFile(contactsPath));
+    const id =
+      defaultId || Number(generateUniqueId({ length: 5, useLetters: false }));
+    contacts.push({ id, name, email, phone });
+    fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
   } catch (err) {
-    return err.message
+    return err.message;
   }
 }
